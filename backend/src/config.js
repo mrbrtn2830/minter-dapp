ee31c71dd322cf723d9fbed35835854f5206cb25
anthony roberts <you@example.com>
2022-04-23 13:03:32 +0100

    m

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const PUBLIC_MINT_START_DATE = "2022-05-01T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-04-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 2000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x10beC8CBe21557f6cE45A2066f67813468ad9dEb"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
